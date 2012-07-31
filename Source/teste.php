979c4f0fb32188b3039ce3ce02910fb47b993dd2
Eduardo <eduardo@ubt-eduardo.(none)>
2012-07-31 13:39:37 -0300

    first commit

---
+++ Source/teste.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 5.0//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br">
+	<head>
+	</head>
+	<style type="text/css">		
+		.aplication				{width:885px; margin:auto;}
+		.faixa					{background: url('blue.png'); height: 300px;}
+		.menu 					{margin-top: 5px; padding: 10px; background: #FFF; float: left; color: #686868;}
+		.menu ul li 			{float: left; margin: 0 10px;}
+		ul li					{margin:0; padding:0;list-style:none;}
+		ul						{list-style:none; margin:0; padding:0;}
+		body 					{margin: 0;}
+	</style>
+	<body>
+
+		<div class="faixa">
+			<div class="content aplication">
+				<div class="menu">
+					<ul>
+						<li>eduardo</li>
+						<li>eduardo</li>
+						<li>eduardo</li>
+						<li>eduardo</li>
+						<li>eduardo</li>
+						<li>eduardo</li>
+						<li>eduardo</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
