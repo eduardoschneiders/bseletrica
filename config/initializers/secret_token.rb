e3776665626625b492347342a2dd567c28d9bba0
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-09-18 16:59:09 -0300

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bseletrica::Application.config.secret_token = 'f10706226446db2cd43db83be39282ef467cdeebccd9a281bf8c843395447f936edc8349553aed5981f77628f3ffce21c801c76088f7a728a47fff4748858bc0'
