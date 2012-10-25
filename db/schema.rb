fbdc3665a49e66bca5b650faac2513a9e5874d89
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-10-25 10:06:51 -0200

    New promotions structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121024163724) do
+ActiveRecord::Schema.define(:version => 20121024172429) do
 
   create_table "promotions", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20121024163724) do
     t.date     "expiration_date"
     t.datetime "created_at",                                     :null => false
     t.datetime "updated_at",                                     :null => false
+    t.string   "image"
   end
 
 end
