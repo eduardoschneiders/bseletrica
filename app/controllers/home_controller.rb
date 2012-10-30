d3e2aa3a2d6ff7889c936b96163967faf1a2cb3e
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-10-30 16:52:10 -0200

    New Services line

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@promotions = Promotion.find(:all, :limit => 5, :order => "created_at desc")
+  	@services = Service.find(:all, :limit => 4, :order => "created_at desc")
   end
 end
