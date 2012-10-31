6fa3f10de265194c0bf6ab7521bb453df1f86f1d
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-10-31 15:19:10 -0200

    Call Suppliers on controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
   	@services = Service.find(:all, :limit => 4, :order => "created_at desc")
   	@our_services = OurService.find(:all, :order => "created_at desc")
   	@clients = Client.find(:all, :limit => 16, :order => "RANDOM()")
+  	@suppliers = Supplier.find(:all, :limit => 7, :order => "RANDOM()")
   end
 end
