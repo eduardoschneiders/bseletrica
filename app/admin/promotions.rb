54b80eabe27c5131c5d733acba013351db368c28
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-11-07 18:05:51 -0200

    Custumized form for Promotions on admin

--- app/admin/promotions.rb
+++ app/admin/promotions.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Promotion do
-  
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs "Details" do
+      f.input :title
+      f.input :image, :as => :file
+    end
+    f.buttons
+  end
 end
