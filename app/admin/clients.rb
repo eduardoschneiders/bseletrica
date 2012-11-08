8251e1e6e3732b9043d7ded883df298fab4a9b4f
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-11-08 11:04:49 -0200

    Adding custom fields on Clients Admin

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Client do
-  
+  form :html => { :enctype => "multipar/form-data" } do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :image, :as => :file
+    end
+    f.buttons
+  end
 end
