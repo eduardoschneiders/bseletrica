b09b2003241bf6dc112dbda0fe8b9827cd3cf5fd
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-11-08 11:03:56 -0200

    Changing from 65px to 75px of with, allowed on Client images

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,4 @@
 class Client < ActiveRecord::Base
   attr_accessible :image, :name
-  has_attached_file :image, :styles => {:small => "65x45"}
+  has_attached_file :image, :styles => {:small => "75x60"}
 end
