d520c7eabc89d732078400aff048bba269ce226f
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-11-08 16:21:07 -0200

    New dimensions set for clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,4 @@
 class Client < ActiveRecord::Base
   attr_accessible :image, :name
-  has_attached_file :image, :styles => {:small => "75x60"}
+  has_attached_file :image, :styles => {:small => "75>x60>"}
 end
