c9168d088e29b8ce2f975c1b10b2055c5c78cbe2
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-11-08 10:43:49 -0200

    Change image field on Clients to work well with Paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121108110555) do
+ActiveRecord::Schema.define(:version => 20121108123831) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -48,9 +48,12 @@ ActiveRecord::Schema.define(:version => 20121108110555) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
-    t.string   "image"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "our_services", :force => true do |t|
