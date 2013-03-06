3e6e2670ff32eb18d8b5ded0507888172e954cd4
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-03-05 23:33:01 -0300

    Included Services into Promotions select; mixed Promotions and Services

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-  	@promotions = Promotion.find(:all, :limit => 5, :order => "created_at desc")
+  	@promotions_highlights = Promotion.find(:all, :limit => 5, :order => "created_at desc")
+  	@services_highlights = Service.find(:all, :limit => 4, :order => "created_at desc")
+  	@highlights = (@promotions_highlights + @services_highlights).sort_by {rand}
   	@services = Service.find(:all, :limit => 4, :order => "created_at desc")
   	@our_services = OurService.find(:all, :order => "created_at desc")
   	@clients = Client.find(:all, :limit => 16, :order => "RANDOM()")
