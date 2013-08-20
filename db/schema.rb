e200fd9b6e68a1c36569282b2bc6fb0ee5b27251
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-08-20 18:00:47 -0300

    Notices structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130820143201) do
+ActiveRecord::Schema.define(:version => 20130820203951) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -54,6 +54,11 @@ ActiveRecord::Schema.define(:version => 20130820143201) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "notices", :force => true do |t|
+    t.string "title"
+    t.string "content"
   end
 
   create_table "our_services", :force => true do |t|
