301102c9c2dae9f213f976fd26aeb1ef73ce65bc
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-08-21 19:10:41 -0300

    Notices list on Home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
   def index
-  	@promotions_highlights = Promotion.find(:all, :limit => 5, :order => "created_at desc")
-  	@services_highlights = Service.find(:all, :limit => 4, :order => "created_at desc")
-  	@highlights = (@promotions_highlights + @services_highlights).sort_by {rand}
-  	@services = Service.find(:all, :limit => 4, :order => "created_at desc")
-  	@our_services = OurService.find(:all, :order => "created_at desc")
-  	@clients = Client.find(:all, :limit => 16, :order => "RANDOM()")
-  	@suppliers = Supplier.find(:all, :limit => 7, :order => "RANDOM()")
+  	@promotions_highlights 	= Promotion.find(:all, :limit => 5, :order => "created_at desc")
+  	@services_highlights 	= Service.find(:all, :limit => 4, :order => "created_at desc")
+  	@highlights 			= (@promotions_highlights + @services_highlights).sort_by {rand}
+  	@services 				= Service.find(:all, :limit => 4, :order => "created_at desc")
+  	@our_services 			= OurService.find(:all, :order => "created_at desc")
+  	@clients 				= Client.find(:all, :limit => 16, :order => "RANDOM()")
+  	@suppliers 				= Supplier.find(:all, :limit => 7, :order => "RANDOM()")
+  	@notices 				= Notice.find(:all, :limit => 6, :order => "updated_at desc")
   end
 end
