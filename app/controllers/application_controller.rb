f189ddaa712b62c62fc7f1a2e3b95a5923918f75
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-08-22 18:52:41 -0300

    Function to short a string with '...' on the end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,25 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   def set_locale
-	  I18n.locale = params[:locale] || I18n.default_locale
-	end
+      I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def shortTitle title, length
+    if title.length >= length
+      sumLetters = 0
+      wordsReturn = []
+      words = title.split(' ')
+      words.each do |word|
+        sumLetters += word.length
+
+        break if sumLetters > length
+        wordsReturn << word
+      end
+      wordsReturn.join(' ') + " ..."
+    else
+      title  
+    end
+  end
+
+  helper_method :shortTitle
 end
