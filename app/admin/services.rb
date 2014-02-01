297085dd27fca43f38d5c0aa2f145735d0c1b8cb
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2014-02-01 18:24:58 -0200

    Allowed multiple file upload for services

--- app/admin/services.rb
+++ app/admin/services.rb
@@ -4,12 +4,12 @@ ActiveAdmin.register Service do
       f.input :title
       f.input :date
       f.input :location
-      f.input :image, :as => :file
+	      f.input :image, :as => :file
     end
 
     f.inputs do
-      f.has_many :asset_for_services, :allow_destroy => true, :new_record => false do |cf|
-        cf.input :image
+      f.has_many :asset_for_services, :multiple => true, :new_record => false do |i|
+	file_field_tag('teste', multiple: true, name: "service[asset_for_services_attributes][][image]")
       end
     end
 
