33169ba7d676126469653c7af2c943b87dadd640
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2014-02-01 19:28:55 -0200

    Added wide_full option to image sizes on services

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ActiveRecord::Base
   attr_accessible :date, :image, :location, :title, :asset_for_services_attributes
-  has_attached_file :image, :styles => {:small => "200x140#", :medium => "290x160#", :wide => "945x200#"}
+  has_attached_file :image, :styles => {:small => "200x140#", :medium => "290x160#", :wide => "945x200#", :wide_full => "900x750"}
 
   has_many :asset_for_services
   accepts_nested_attributes_for :asset_for_services
