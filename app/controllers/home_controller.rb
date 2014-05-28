c7ce23775d6b6904fa87a735a39b570d30bc94eb
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-05-28 10:18:33 -0300

    Change Our Services

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,14 @@ class HomeController < ApplicationController
     @services_highlights    = Service.find(:all, :limit => 4, :order => "created_at desc")
     @highlights             = (@promotions_highlights + @services_highlights).sort_by {rand}
     @services               = Service.find(:all, :limit => 4, :order => "created_at desc")
-    @our_services           = OurService.find(:all, :order => "created_at desc")
+    @our_services           = [
+                                { title: 'Instalações Elétricas', image: 'instalacoes.jpg'},
+                                { title: 'SPDA', image: 'spda.jpg' },
+                                { title: 'Redes Lógicas', image: 'redes.jpg'},
+                                { title: 'Painéis Elétricos', image: 'paineis.jpg'},
+                                { title: 'Grupos Geradores', image: 'grupos.jpg' },
+                                { title: 'Controle de Fator de Potência', image: 'controle.jpg' }
+                              ]
     @clients                = Client.find(:all, :limit => 16, :order => "RANDOM()")
     @suppliers              = Supplier.find(:all, :limit => 7, :order => "RANDOM()")
     @notices                = Notice.find(:all, :limit => 6, :order => "updated_at desc")
